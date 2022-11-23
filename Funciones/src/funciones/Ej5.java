dc57ee794cc1e0e90050cebfd2b2c3af62802ee5
lcardozo <lcardozo@106-25.info2>
2022-11-23 14:23:52 +0100

    (:

--- Funciones/src/funciones/Ej5.java
+++ Funciones/src/funciones/Ej5.java
@@ -7,28 +7,36 @@ public class Ej5 {
  *  Diseña un método main() que pruebe el funcionamiento de dicha función.*/
 	
 	static boolean esVocal (String letra) {
-		boolean resultado = false;
-		
+		//Iniciamos una funcion donde la comparacion la iniciaremos a false para que todo lo que no este en el if sea falso
+		boolean comparacion = false;
+		/*Iniciamos un if donde si cumple que el string es alguna de las vocales la variable comparacion cambie a true*/
 		if (letra.equals("a")||letra.equals("e")||letra.equals("i")||letra.equals("o")||letra.equals("u")) {
 			
-			resultado = true;
+			comparacion = true;
 		}
-			return resultado;
+		//Solicitamos que la funcion devuelva comparacion
+			return comparacion;
 	}
 	public static void main(String[] args) {
-		//Declaramos la variables
+		//Declaramos la variables donde la variable es nos dará el resultado de la comparacion
 		boolean es;
-		String letraUser;
+		String letraUser;//y en letra user guasrdaremos lo que introducirá el usuario
 		
 		//iniciamos el Scanner
 		Scanner lee = new Scanner(System.in);
 		 
+		//imprimimos la solicitud y leemos lo que introducirá el usuario
 		System.out.println("Por favor introduzca una letra: ");
 		letraUser = lee.nextLine();
 		
+		//En la variable del resultado en este caso "es" llamamos la funcion y le metemos como parametro lo que introducirá el usuario
 		es = esVocal(letraUser);
 		
 		System.out.println("La letra introducida es vocal? : " + es);
+	
+	//cerramos el scanner
+		lee.close();
+	
 	}
 
 }
